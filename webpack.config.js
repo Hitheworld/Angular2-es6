8d13365f0752bc964bf2c7cf09e83d35840f0f74
Hitheworld <947508757@qq.com>
2016-10-06 21:45:21 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var config = {},
-	cleanFolder = function(config) {
-		var exec = require('child_process').exec,
-			clear = config.clearBeforeBuild;
-		clear && exec('rm -rf ' + config.output.path, function(err, out) {
-			console.log(out);
-			err && console.error(err);
-		});
-	};
+    cleanFolder = function(config) {
+        var exec = require('child_process').exec,
+            clear = config.clearBeforeBuild;
+        clear && exec('rm -rf ' + config.output.path, function(err, out) {
+            console.log(out);
+            err && console.error(err);
+        });
+    };
 
 config = require("./webpack.dev.config.js");
 
